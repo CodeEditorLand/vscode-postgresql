0cbfe603e27a047aed1d7c1073cd0c461c0d764d
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-17 16:35:58 +0200

    squash!

--- Source/prompts/password.ts
+++ Source/prompts/password.ts
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
 // This code is originally from https://github.com/DonJayamanne/bowerVSCode
 // License: https://github.com/DonJayamanne/bowerVSCode/blob/master/LICENSE
-import InputPrompt from './input';
+import InputPrompt from "./input";
 
 export default class PasswordPrompt extends InputPrompt {
+	constructor(question: any, ignoreFocusOut?: boolean) {
+		super(question, ignoreFocusOut);
 
-    constructor(question: any, ignoreFocusOut?: boolean) {
-        super(question, ignoreFocusOut);
-
-        this._options.password = true;
-    }
+		this._options.password = true;
+	}
 }
